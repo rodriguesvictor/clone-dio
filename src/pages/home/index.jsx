49845966243595060c1bd33acb7702416a52f97d
Victor Rodrigues <Victordarkiin@gmail.com>
2022-11-07 17:47:38 -0300

    Criação e estilização de componentes da HomePage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,34 @@
 import { Link }  from "react-router-dom";
+
+import bannerImage from "../../assets/banner.png";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+
+import { Container, TextContent, Title, TitleHighLight } from "./styles";
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Container>
+				<div>
+					<Title>
+						<TitleHighLight>
+						Implemente
+						<br />
+						</TitleHighLight>
+						o seu futuro global agora!
+					</Title>
+					<TextContent>
+						Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare o seu novo
+						desafio profissional, evoluindo em comunidade com os melhores experts.
+					</TextContent>
+					<Button title="Começar agora!" variant="secondary" onClick={() => null} />
+				</div>
+				<div>
+					<img src={bannerImage} alt="Imagem Principal" />
+				</div>
+			</Container>
 		</>
 	);
 };
