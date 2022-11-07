ee5d0b78e8e831a92126625accf4b0e7c17b8990
Victor Rodrigues <Victordarkiin@gmail.com>
2022-11-07 18:57:54 -0300

    Criação e estilização da LoginPage

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -5,4 +5,21 @@ export const InputContainer = styled.div`
 	max-width: 275px;
 	height: 30px;
 	border-bottom: 1px solid #3B3450;
+
+	display: flex;
+	align-items: center;
+	margin-bottom: 20px;
+`
+
+export const IconContainer = styled.div`
+	margin-right: 10px;
+`
+
+export const InputText = styled.input`
+	background-color: transparent;
+	color: #FFF;
+	width: 100%;
+	border: none;
+	height: 30px;
+
 `
