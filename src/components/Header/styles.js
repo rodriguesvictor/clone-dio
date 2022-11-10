162a457ac3056b8db539600f333b8993e8f2dfb6
Victor Rodrigues <Victordarkiin@gmail.com>
2022-11-10 00:18:56 -0300

    Estilo da Header atualizado

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -43,13 +43,13 @@ export const BuscarInputContainer = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	
+
 `
 
 export const Menu = styled.a`
-	font-family: 'Open Sans ',
+	font-family: 'Open Sans';
 	font-style: normal;
-	font-size: 12px;
+	font-size: 18px;
 	line-height: 25px;
 	color: #FFF;
 	margin-right: 12px;
@@ -57,7 +57,7 @@ export const Menu = styled.a`
 `
 
 export const MenuRight = styled.a`
-	font-family: 'Open Sans ',
+	font-family: 'Open Sans';
 	font-style: normal;
 	font-size: 12px;
 	line-height: 25px;
