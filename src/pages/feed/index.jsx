4b6c444b8e69563bc41077edaba0951a8d3e5f43
Victor Rodrigues <Victordarkiin@gmail.com>
2022-11-10 00:05:34 -0300

    Adição do UserInfo + Atualização da FeedPage

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -1,15 +1,31 @@
 import { Link }  from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Card } from "../../components/Card";
+import { UserInfo } from "../../components/UserInfo";
 
-import { Container, TextContent, Title, TitleHighLight } from "./styles";
+import { Container, Column, Title, TitleHighLight } from "./styles";
 
 const Feed = () => {
 	return (
 		<>
 			<Header />
 			<Container>
-				<Card />
+				<Column flex={3}>
+					<Title>Feed</Title>
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</Column>
+				<Column flex={1}>
+					<TitleHighLight># RANKING TOP 5 DA SEMANA</TitleHighLight>
+					<UserInfo percentual={80} name="Victor Rodrigues" image="https://avatars.githubusercontent.com/u/57380118?v=4" />
+					<UserInfo percentual={27} name="Victor Rodrigues" image="https://avatars.githubusercontent.com/u/57380118?v=4" />
+					<UserInfo percentual={10} name="Victor Rodrigues" image="https://avatars.githubusercontent.com/u/57380118?v=4" />
+					<UserInfo percentual={50} name="Victor Rodrigues" image="https://avatars.githubusercontent.com/u/57380118?v=4" />
+				</Column>
 			</Container>
 		</>
 	);
--- src/pages/feed/styles.js
+++ src/pages/feed/styles.js
@@ -1,40 +1,37 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-	width:100%;
+	width: 100%;
 	max-width: 80%;
 	margin: 0 auto;
 	margin-top: 120px;
 
 	display: flex;
+	flex-direction: row;
 	justify-content: space-between;
-	align-items: center;
 `
 
-export const Title = styled.h2`
+export const Title = styled.h3`
 	font-family: 'Open Sans';
 	font-style: normal;
 	font-weight: 700;
-	font-size: 32px;
-	width: 320px;
-	margin-bottom: 20px;
-	line-height: 44px;
-
+	font-size: 18px;
+	margin-bottom: 24px;
+	line-height: 25px;
 	color: #FFF;
 `
 
-export const TitleHighLight = styled.span`
-	color: #E4105D;
-`
-
-export const TextContent = styled.p`
+export const TitleHighLight = styled.h3`
 	font-family: 'Open Sans';
 	font-style: normal;
-	font-weight: 400;
-	font-size: 16px;
-	width: 420px;
-	margin-bottom: 20px;
-	line-height: 22px;
+	font-weight: 700;
+	font-size: 18px;
+	margin-bottom: 24px;
+	line-height: 25px;
+	color: #FFFFFF70;
+`
 
-	color: #FFF;
+export const Column = styled.div`
+	flex: ${({flex}) => flex};
+	padding-right: 24px;
 `
