6b73363d550d9a717b4d16b8e96f192c934a4f1c
Victor Rodrigues <Victordarkiin@gmail.com>
2022-11-11 15:27:54 -0300

    Adição de validação de form com yup

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { MdEmail, MdLock } from "react-icons/md";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-import { MdEmail, MdLock } from 'react-icons/md';
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
@@ -17,20 +20,40 @@ import {
 	Wrapper,
 } from "./styles";
 
+const schema = yup
+	.object({
+		email: yup.string().email('Email inválido').required('Campo obrigatório'),
+		password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatório'),
+	})
+	.required();
+
 const Login = () => {
 	const navigate = useNavigate();
 
+	const {
+		control,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm({
+		resolver: yupResolver(schema),
+		mode: "onChange",
+	});
+
+	console.log(isValid, errors);
+	const onSubmit = (data) => console.log(data);
+
 	const handleClickSignIn = () => {
-		navigate('/feed');
-	}
+		navigate("/feed");
+	};
 	return (
 		<>
 			<Header />
 			<Container>
 				<Column>
 					<Title>
-						A plataforma para você aprender com experts, dominar as principais tecnologias
-						e encontrar mais rápido nas empresas mais desejadas.
+						A plataforma para você aprender com experts, dominar as
+						principais tecnologias e encontrar mais rápido nas
+						empresas mais desejadas.
 					</Title>
 				</Column>
 				<Column>
@@ -39,10 +62,27 @@ const Login = () => {
 						<SubTitleLogin>
 							Faça seu login e make the change._
 						</SubTitleLogin>
-						<form>
-							<Input placeholder="E-mail" leftIcon={<MdEmail/>}/>
-							<Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
-							<Button title="Entrar" variant="secondary" onClick={handleClickSignIn} type="button" />
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<Input
+								name="email"
+								errorMessage={errors?.email?.message}
+								control={control}
+								placeholder="E-mail"
+								leftIcon={<MdEmail />}
+							/>
+							<Input
+								name="password"
+								errorMessage={errors?.password?.message}
+								control={control}
+								placeholder="Senha"
+								type="password"
+								leftIcon={<MdLock />}
+							/>
+							<Button
+								title="Entrar"
+								variant="secondary"
+								type="submit"
+							/>
 						</form>
 						<Row>
 							<EsqueciText>Esqueci minha senha</EsqueciText>
